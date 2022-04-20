5337e781bc8734ad4b944eb7a3862f448a1587ed
khanshorif331 <khanshorif331@gmail.com>
2022-04-20 20:25:20 +0600

    updated dynamic title and email verified

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -11,6 +11,7 @@ import { async } from '@firebase/util'
 import Loading from '../../Shared/Loading/Loading'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import PageTitle from '../../Shared/PageTitle/PageTitle'
 
 const Login = () => {
 	const [signInWithEmailAndPassword, user, loading, error] =
@@ -61,6 +62,7 @@ const Login = () => {
 
 	return (
 		<div className='container w-50 mx-auto'>
+			<PageTitle title='Login'></PageTitle>
 			<h2 className='text-primary text-center mt-2'>Please Login</h2>
 			<Form onSubmit={handleSubmit}>
 				<Form.Group className='mb-3' controlId='formBasicEmail'>
--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,10 +1,17 @@
+// import { sendEmailVerification } from 'firebase/auth'
 import React from 'react'
-import { useAuthState } from 'react-firebase-hooks/auth'
+import {
+	useAuthState,
+	useSendEmailVerification,
+} from 'react-firebase-hooks/auth'
 import { Navigate, useLocation } from 'react-router-dom'
+import { toast, ToastContainer } from 'react-toastify'
 import auth from '../../../firebase.init'
 import Loading from '../../Shared/Loading/Loading'
 
 const RequireAuth = ({ children }) => {
+	const [sendEmailVerification, sending, error] =
+		useSendEmailVerification(auth)
 	const [user, loading] = useAuthState(auth)
 	const location = useLocation()
 	if (loading) {
@@ -12,6 +19,24 @@ const RequireAuth = ({ children }) => {
 	}
 	if (!user) {
 		return <Navigate to='/login' state={{ from: location }} replace />
+	}
+	if (!user.emailVerified) {
+		return (
+			<div>
+				<h1 className='text-danger'> Your Email is not verified.!!</h1>
+				<h3 className='text-success'>Please verify your email address.</h3>
+				<button
+					className='btn btn-primary'
+					onClick={async () => {
+						await sendEmailVerification()
+						toast('Sent email')
+					}}
+				>
+					Send Code Again
+				</button>
+				<ToastContainer></ToastContainer>
+			</div>
+		)
 	}
 	return children
 }
