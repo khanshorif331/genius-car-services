46fd6c25d132a827c74bde4178605312b305e74c
khanshorif331 <khanshorif331@gmail.com>
2022-04-30 00:13:06 +0600

    added another section

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -12,6 +12,7 @@ import Loading from '../../Shared/Loading/Loading'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import PageTitle from '../../Shared/PageTitle/PageTitle'
+import axios from 'axios'
 
 const Login = () => {
 	const [signInWithEmailAndPassword, user, loading, error] =
@@ -31,12 +32,18 @@ const Login = () => {
 
 	const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth)
 
-	const handleSubmit = event => {
+	const handleSubmit = async event => {
 		event.preventDefault()
 		const email = emailRef.current.value
 		const password = passwordRef.current.value
 
-		signInWithEmailAndPassword(email, password)
+		await signInWithEmailAndPassword(email, password)
+		const { data } = await axios.post('http://localhost:5000/login', {
+			email,
+		})
+		localStorage.setItem('accessToken', data.accessToken)
+		navigate(from, { replace: true })
+		// console.log(data)
 	}
 
 	const navigateRegister = event => {
@@ -57,7 +64,7 @@ const Login = () => {
 	}
 
 	if (user) {
-		navigate(from, { replace: true })
+		// navigate(from, { replace: true })
 	}
 
 	return (
